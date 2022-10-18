15e73de7091b671c50b3e470023bbc2c112dc68c
ngwueche <94566742+Ngwueche@users.noreply.github.com>
2022-10-18 07:27:29 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,85 +1,61 @@
-//Get all transactions
-// Add a new transaction
-// Update an existing Transaction
-//Delete A transaction
-// Get A transaction by Id
-// Get All transactions by type (Withdraw/ Deposit)
-// Bonus: Get all transactions done on a particular date
-
 let {dataStore} = require('./txn')
 
+// Get the timeStamp from dataStore
+const unixDateObject = dataStore.filter((element, index) =>{
+    return element.TimeStamp;
+})
+
+const unixTimestamp = unixDateObject;
+const milliseconds = unixTimestamp * 1000
+const dateObject = new Date(milliseconds)
+const humanDateFormat = dateObject.toLocaleString()
+
+
+ 
+const newTransaction = {
+    "TimeStamp" : "1571967200",
+    "TransactionType" : "DEPOSIT",
+    "Amount" : "10.683640",
+    "Token" : "ETH"
+}
 //Get all transactions
 const getAllTransactions = ()=> dataStore
+
+// Add a transaction
+const addTransaction = () => dataStore.push(newTransaction)
 
 // Add a delete transaction
 const deleteTransaction = () => dataStore.pop({});
 
-// Add a new transaction
-const  addNewTransaction = () => dataStore.push({});
-// Add a new transaction
-
-const  addNewTransaction = () => dataStore.push({});
-
-
-// Update an existing Transaction
-const updateTransaction = (element) => {
+// Get A transaction by Id
+const updateTransaction = () => {
  for(let i =0; i< len; i++){
     dataStore.timeStamp ==="";
-
  }
 
 }
+// Update an existing Transaction
+
+// Get All transactions by type (Withdraw/  Deposit ✅ )
+const depositTransaction = dataStore.filter((element, index) =>{
+        return element.TransactionType === 'DEPOSIT' 
+})
+
+
+// Bonus: Get all transactions done on a particular date
+const transactionByDate = dataStore.filter((element, index)=>{
+    
+});
+
 // Get length of the array
 const len = dataStore.length;
 console.log(len)
 
-// Get array of Transaction of deposit type
-const depositTransaction = dataStore.filter((element, index) =>{
-        return element.TransactionType === 'WITHDRAWAL' 
-})
-// console.log(depositTransaction)
-
-
 // Total amount of depositTransaction
 const totalDeposit = depositTransaction.filter((element, index) =>{
     return element.Token ===  "XRP"
-    return element.Token ===  "ETH"
-    return element.Token ===  "BTC"
+    // return element.Token ===  "ETH"
+    // return element.Token ===  "BTC"
     
 }) 
-// console.log(totalDeposit)
-
-// Total amount of withdrawalTransaction
-
-// const addNewTransaction = () =>{
-//     return dataStore.push({
-//         "TimeStamp" : "157196718",
-//         "TransactionType" : "WITHDRAWAL",
-//         "Amount" : "10.8660",
-//         "Token" : "BTC"
-//     })
-// }
-// const deleteTransaction = () =>{
-//     return dataStore.pop[0]
-// }
-
-// // Get A transaction by Id
-// const getTransactionById = ()=>{
-//     return dataStore.find({'TimeStamp':'1571967110'})
-// }
-
-// // Get All transactions by type (Withdraw/ Deposit)
-// const getTransactionByType = ()=>{
-//     return dataStore.find({'TransactionType':'DEPOSIT'})
-//     }
-
-// //Get all transactions done on a particular date
-// const timeStamp = () =>{
-//    return dataStore.find({'TimeStamp'}).toLocaleTimeString()
-
-
-// console.log(deleteTransaction())
-
-// // arrayMethods
-// // 
-// // 
+console.log(totalDeposit)
